d353b60801b4ef0272bf98d153e57e7b4b5ce5cb
Jabez Tho <jabeztho@gmail.com>
2020-07-03 10:23:25 +0800

    retest buikld amplify configure bug

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,13 @@ import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  "aws_project_region": "us-east-1",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_ods9jpAIR",
+  "aws_user_pools_web_client_id": "7jdh37dljq5oehhn47l56r2m98",
+  "oauth": {}
+});
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent, MissionComponent],
