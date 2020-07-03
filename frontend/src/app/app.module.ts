a11c3ad32315376a03ffc690b92df88d48952d6e
Jabez Tho <jabeztho@gmail.com>
2020-07-03 10:34:22 +0800

    commented amplify bug

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import awsconfig from './aws-exports';
 import { MapComponent } from './map/map.component';
 import { DoorComponent } from './door/door.component';
 
+<<<<<<< HEAD
 Amplify.configure({
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
@@ -22,6 +23,15 @@ Amplify.configure({
   aws_user_pools_web_client_id: '7jdh37dljq5oehhn47l56r2m98',
   oauth: {},
 });
+=======
+// Amplify.configure({
+//   "aws_project_region": "us-east-1",
+//   "aws_cognito_region": "us-east-1",
+//   "aws_user_pools_id": "us-east-1_ods9jpAIR",
+//   "aws_user_pools_web_client_id": "7jdh37dljq5oehhn47l56r2m98",
+//   "oauth": {}
+// });
+>>>>>>> commented amplify bug
 
 @NgModule({
   declarations: [
