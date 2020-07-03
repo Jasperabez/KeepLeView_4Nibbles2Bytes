59ad87f75e5449863b81dfd332c0fd41f0560b6d
Hanscau <hanscau@hotmail.com>
2020-07-03 10:31:59 +0800

    Fixed undefined global

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,15 +14,21 @@ import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 Amplify.configure({
-  "aws_project_region": "us-east-1",
-  "aws_cognito_region": "us-east-1",
-  "aws_user_pools_id": "us-east-1_ods9jpAIR",
-  "aws_user_pools_web_client_id": "7jdh37dljq5oehhn47l56r2m98",
-  "oauth": {}
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_ods9jpAIR',
+  aws_user_pools_web_client_id: '7jdh37dljq5oehhn47l56r2m98',
+  oauth: {},
 });
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent, MissionComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    MissionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
