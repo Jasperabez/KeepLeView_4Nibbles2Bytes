a678ba7ee470fec4d415565d7e9d0f4b1a63c625
Hanscau <hanscau@hotmail.com>
2020-07-16 11:52:05 +0800

    Implemented quest passing user location

--- frontend/src/app/quest/quest.component.ts
+++ frontend/src/app/quest/quest.component.ts
@@ -2,9 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { QuestService, AuthenticationService } from '@/services';
+import {
+  QuestService,
+  AuthenticationService,
+  LocationService,
+} from '@/services';
 
-import { Quest } from '@/models';
+import { Quest, Coords } from '@/models';
 
 @Component({
   selector: 'app-mission',
@@ -15,12 +19,14 @@ export class QuestComponent implements OnInit {
   questId: string;
   userId: string;
   quest: Quest;
+  userLocation: Coords;
   isLoading = true;
   constructor(
     private router: ActivatedRoute,
     private questService: QuestService,
     private location: Location,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private locationService: LocationService
   ) {}
 
   ngOnInit(): void {
@@ -29,6 +35,11 @@ export class QuestComponent implements OnInit {
     this.questService.getById(this.questId).then((quest) => {
       this.quest = quest;
       this.isLoading = false;
+    });
+    this.locationService.getPosition().then((location) => {
+      this.userLocation = new Coords();
+      this.userLocation.lat = location.coords.latitude;
+      this.userLocation.lng = location.coords.longitude;
     });
   }
 
